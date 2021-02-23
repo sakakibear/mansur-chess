cfd17f8561d09a4d1ed3cab74352a0a7acdd2a51
sakakibear <song.st.87@gmail.com>
2021-02-23 17:50:04 +0900

    [#20] Base class of Board and Move.

---
+++ game/BaseMove.java
@@ -0,0 +1,9 @@
+package game;
+
+/**
+ * Move of a game. Data structure should be defined in subclasses.
+ */
+public abstract class BaseMove {
+    @Override
+    public abstract String toString();
+}
