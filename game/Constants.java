7ddc959dcd847947b450e2194f2d6f0b4d1dbf5c
sakakibear <song.st.87@gmail.com>
2021-02-23 15:43:33 +0900

    [#20] Constant class for BaseGame.

---
+++ game/Constants.java
@@ -0,0 +1,14 @@
+package game;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final int PLAYER_1 = 1;
+    public static final int PLAYER_2 = 2;
+
+    public static final int DEFAULT_SEARCH_DEPTH = 5;
+    public static final int VALUE_LOWER_BOUND = -10001;
+    public static final int VALUE_UPPER_BOUND = 10001;
+}
