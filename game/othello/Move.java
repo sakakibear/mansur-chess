7380e361f404dd8671d2873e6ea759915834423a
sakakibear <song.st.87@gmail.com>
2021-08-29 16:03:57 +0900

    [#19] Move class of Othello game.

---
+++ game/othello/Move.java
@@ -0,0 +1,52 @@
+package game.othello;
+
+import static game.othello.Constants.PIECES;
+
+import game.BaseMove;
+
+/**
+ * A player's move in an Othello game.
+ */
+public class Move extends BaseMove {
+
+    private int x;
+    private int y;
+    private int player;
+    private boolean isPass;
+
+    public Move(int x, int y, int player) {
+        this.x = x;
+        this.y = y;
+        this.player = player;
+        this.isPass = false;
+    }
+
+    // Pass move
+    public Move(int player) {
+        this.x = -1;
+        this.y = -1;
+        this.player = player;
+        this.isPass = true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%c] > %c%d", PIECES[player], PIECES[player], (char) ('a' + y), (x + 1));
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getPlayer() {
+        return player;
+    }
+
+    public boolean isPass() {
+        return isPass;
+    }
+}
