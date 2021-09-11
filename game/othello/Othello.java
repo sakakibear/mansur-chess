750e3dfda78d8aa23a5938e79473d2d129bdafd3
sakakibear <song.st.87@gmail.com>
2021-09-11 23:49:59 +0900

    [#19] Delegate rule related functions to Rule class.

--- game/othello/Othello.java
+++ game/othello/Othello.java
@@ -5,10 +5,8 @@ import static game.Constants.PLAYER_2;
 import static game.othello.Constants.BOARD_SIZE;
 import static game.othello.Constants.DARK;
 import static game.othello.Constants.DISCS;
-import static game.othello.Constants.EMPTY;
 import static game.othello.Constants.LIGHT;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -16,14 +14,15 @@ import java.util.Scanner;
 import game.BaseGame;
 import game.BaseMove;
 
+/**
+ * TODO Use generic instead of casting like (Board) board.
+ */
 public class Othello extends BaseGame {
+
+    protected Rule rule = Rule.getInstance();
 
     // Scanner to get user input
     protected Scanner scanner;
-
-    // 8 directions on the board
-    protected static int[][] dirs = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 },
-            { 1, 1 }, };
 
     // XXX for test
     @Override
@@ -66,7 +65,6 @@ public class Othello extends BaseGame {
     public void init(String[] args) {
         super.init(args);
         Board othelloBoard = new Board();
-        // TODO Use generic.
         board = othelloBoard;
         othelloBoard.init();
         // TODO evaluator
@@ -75,81 +73,17 @@ public class Othello extends BaseGame {
 
     @Override
     protected boolean isGameOver(int player) {
-        List<Move> moves1 = getValidMoves(PLAYER_1);
-        if (moves1.size() >= 1 && !moves1.get(0).isPass())
-            return false;
-        List<Move> moves2 = getValidMoves(PLAYER_2);
-        if (moves2.size() >= 1 && !moves2.get(0).isPass())
-            return false;
-        return true;
+        return rule.isGameOver((Board) board);
     }
 
     @Override
     protected List<Move> getValidMoves(int player) {
-        // TODO Check game over first
-        Board b = (Board) board;
-        List<Move> result = new ArrayList<Move>();
-        for (int i = 0; i < BOARD_SIZE; i++)
-            for (int j = 0; j < BOARD_SIZE; j++)
-                if (isValidMove(b, player, i, j))
-                    result.add(new Move(i, j, player));
-        // Add a pass move if no moves could be done
-        if (result.size() == 0)
-            result.add(new Move(player));
-        return result;
-    }
-
-    protected boolean isValidMove(Board b, int player, int x, int y) {
-        if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE)
-            return false;
-        if (b.get(x, y) != EMPTY)
-            return false;
-        int self = player == PLAYER_1 ? DARK : LIGHT;
-        int oppo = player == PLAYER_1 ? LIGHT : DARK;
-        for (int[] dir : dirs) {
-            int i = x + dir[0], j = y + dir[1];
-            boolean hasOppo = false;
-            while (i >= 0 && i < BOARD_SIZE && j >= 0 && j < BOARD_SIZE && b.get(i, j) == oppo) {
-                hasOppo = true;
-                i += dir[0];
-                j += dir[1];
-            }
-            if (hasOppo && i >= 0 && i < BOARD_SIZE && j >= 0 && j < BOARD_SIZE && b.get(i, j) == self)
-                return true;
-        }
-        return false;
+        return rule.getMoves((Board) board, player);
     }
 
     @Override
     protected void move(BaseMove move) {
-        // TODO Use generic
-        Move m = (Move) move;
-        if (m.isPass())
-            return;
-        Board b = (Board) board;
-        int player = m.getPlayer();
-        int x = m.getX();
-        int y = m.getY();
-        int self = player == PLAYER_1 ? DARK : LIGHT;
-        int oppo = player == PLAYER_1 ? LIGHT : DARK;
-        b.set(x, y, self);
-        for (int[] dir : dirs) {
-            int i = x + dir[0], j = y + dir[1];
-            int cnt = 0;
-            while (i >= 0 && i < BOARD_SIZE && j >= 0 && j < BOARD_SIZE && b.get(i, j) == oppo) {
-                cnt++;
-                i += dir[0];
-                j += dir[1];
-            }
-            if (i >= 0 && i < BOARD_SIZE && j >= 0 && j < BOARD_SIZE && b.get(i, j) == self) {
-                int ii = x + dir[0], jj = y + dir[1];
-                for (int k = 0; k < cnt; k++) {
-                    b.set(ii, jj, self);
-                    ii += dir[0];
-                    jj += dir[1];
-                }
-            }
-        }
+        rule.takeMove((Board) board, (Move) move);
     }
 
     @Override
@@ -180,7 +114,7 @@ public class Othello extends BaseGame {
                 cy = tmp;
             }
             int x = cx - '1', y = cy - 'a';
-            if (isValidMove((Board) board, player, x, y))
+            if (rule.isValidMove((Board) board, player, x, y))
                 return new Move(x, y, player);
         }
     }
