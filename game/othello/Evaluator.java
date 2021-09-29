923575b5bab023dc69872744f04577f759c29fd8
sakakibear <song.st.87@gmail.com>
2021-09-29 12:08:20 +0900

    [#25] Evaluate discs in corners higher than others.

--- game/othello/Evaluator.java
+++ game/othello/Evaluator.java
@@ -1,8 +1,11 @@
 package game.othello;
 
 import static game.othello.Constants.BOARD_SIZE;
+import static game.othello.Constants.CORNER_POINT;
 import static game.othello.Constants.DARK;
+import static game.othello.Constants.EMPTY;
 import static game.othello.Constants.LIGHT;
+import static game.othello.Constants.UNSTABLE_X_DEDUCTION;
 import static game.othello.Constants.VALUE_LOSE;
 import static game.othello.Constants.VALUE_WIN;
 
@@ -36,7 +39,29 @@ public class Evaluator extends BaseEvaluator {
             else
                 return VALUE_LOSE;
         }
-        return darkCnt - lightCnt;
+        return darkCnt - lightCnt + 0;
     }
 
+    protected int getCornerAdjustment(Board board) {
+        int res = 0;
+        res += getCornerValue(board.get(0, 0), board.get(1, 1));
+        res += getCornerValue(board.get(0, 7), board.get(1, 6));
+        res += getCornerValue(board.get(7, 0), board.get(6, 1));
+        res += getCornerValue(board.get(7, 7), board.get(6, 6));
+        return res;
+    }
+
+    protected int getCornerValue(int corner, int x) {
+        if (corner == EMPTY) {
+            if (x == DARK)
+                return -UNSTABLE_X_DEDUCTION;
+            else if (x == LIGHT)
+                return UNSTABLE_X_DEDUCTION;
+            return 0;
+        }
+        if (corner == DARK)
+            return CORNER_POINT;
+        else
+            return -CORNER_POINT;
+    }
 }
