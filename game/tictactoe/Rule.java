78b481c6d8786207de276cac855bfb233b4cddc7
sakakibear <song.st.87@gmail.com>
2026-03-29 23:37:22 +0900

    [#57] Implement `getWinner` in `tictactoe.Rule`.

--- game/tictactoe/Rule.java
+++ game/tictactoe/Rule.java
@@ -113,4 +113,60 @@ public class Rule implements GameRule<Move, Player, Board> {
         }
         return true;
     }
+
+    public Player getWinner(Board board) {
+        int[] row = new int[BOARD_SIZE];
+        Player rowWinner = null;
+
+        // check horizontal rows
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                row[j] = board.get(i, j);
+            }
+            rowWinner = getWinnerOfRow(row);
+            if (rowWinner != null)
+                return rowWinner;
+        }
+
+        // check vertical rows
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            for (int i = 0; i < BOARD_SIZE; i++) {
+                row[i] = board.get(i, j);
+            }
+            rowWinner = getWinnerOfRow(row);
+            if (rowWinner != null)
+                return rowWinner;
+        }
+
+        // check diagonal rows
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            row[i] = board.get(i, i);
+        }
+        if (rowWinner != null)
+            return rowWinner;
+
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            row[i] = board.get(i, BOARD_SIZE - i - 1);
+        }
+        rowWinner = getWinnerOfRow(row);
+        if (rowWinner != null)
+            return rowWinner;
+
+        return null;
+    }
+
+    protected Player getWinnerOfRow(int[] row) {
+        int cnt_player_1 = 0, cnt_player_2 = 0;
+        for (int piece : row) {
+            if (piece == Player.PLAYER_1.getId())
+                cnt_player_1++;
+            else if (piece == Player.PLAYER_2.getId())
+                cnt_player_2++;
+        }
+        if (cnt_player_1 == BOARD_SIZE)
+            return Player.PLAYER_1;
+        if (cnt_player_2 == BOARD_SIZE)
+            return Player.PLAYER_2;
+        return null;
+    }
 }
